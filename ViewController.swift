dd3d6f7081822411a9b39b7815b1c523c6e6f10e
cbradley2b <cbradley2b@gmail.com>
2017-06-17 16:45:04 -0400

    actions and outlets lession

--- ViewController.swift
+++ ViewController.swift
@@ -9,9 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textDisplay: UILabel!
 
+    @IBAction func helloWorld(_ sender: Any) {
+        textDisplay.text = "Hello World"
+        print("Pressed the Hello World Button")
+    }
+    
+    @IBAction func goodbyeWorld(_ sender: Any) {
+        textDisplay.text = "Goodbye World"
+        print("pressed the goodbye world button")
+    }
+    
+    
+    @IBAction func unicorn(_ sender: Any) {
+        textDisplay.text = "🦄"
+        print("pressed the unicorn button")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textDisplay.text = "Zzzz..."
     }
 
     override func didReceiveMemoryWarning() {
